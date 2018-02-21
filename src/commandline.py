082a10ceb3e717e59006c73a4107b266f3cf38aa
Mariyana Levova <mariyana.levova@ucdconnect.ie>
2018-02-21 20:24:44 +0000

    added commandline option

---
+++ src/commandline.py
@@ -0,0 +1,10 @@
+import systeminfo
+from systeminfo import main as m
+
+def main():
+    output = m.get_info()
+    print(output)
+    return
+
+if __name__ == '__main__':
+    main()
--- src/flask_platform/views.py
+++
@@ -1,6 +0,0 @@
-from systeminfo import main as sysinfo
-
-@app.route('/)
-def get_system_info():
-    return "The platform is "+ sysinfo.get_platform_info()
-
