c6faa681f055aa8ba3ed2de4b574124c037db92a
MariyanaLevova <35726481+MariyanaLevova@users.noreply.github.com>
2018-02-22 09:11:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 # Add here console scripts and other entry points in ini-style format
 entry_points = {
-    'console_scripts':[SE_A2=flask_platform..src.commandline:main']
+    'console_scripts':[SE_A2=flask_platform.src.commandline:main']
 }
 
 
